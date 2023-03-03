1f39cfa6ca95558e2207e4453b20f5b20d048bb0
Alimsky on work <fiveminu@mail.ru>
2023-03-03 17:35:06 +0600

    task1-1

--- task1/src/task1.java
+++ task1/src/task1.java
@@ -3,6 +3,8 @@ import jakarta.servlet.annotation.*;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 @WebServlet(name = "task1", value = "/task1")
 public class task1 extends HttpServlet {
@@ -10,10 +12,39 @@ public class task1 extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
-        for (int i = 0; i < 10; i++) {
-            out.print("<p>HELLO BITLAB</p>");
+        ArrayList<User> users = new ArrayList<User>();
+
+        users.add(new User("Alim1", "Raisov1", "it", 500000));
+        users.add(new User("Alim2", "Raisov2", "it", 600000));
+        users.add(new User("Alim3", "Raisov3", "it", 700000));
+        users.add(new User("Alim4", "Raisov4", "it", 800000));
+        users.add(new User("Alim5", "Raisov5", "it", 900000));
+
+        out.print("<table cellpadding='10px'>");
+        out.print("<tr>");
+            out.print("<th>");
+            out.print("Name");
+            out.print("</th>");
+            out.print("<th>");
+            out.print("Surname");
+            out.print("</th>");
+            out.print("<th>");
+            out.print("Department");
+            out.print("</th>");
+            out.print("<th>");
+            out.print("Salary");
+            out.print("</th>");
+        out.print("</tr>");
+
+        for (User user: users) {
+            out.print("<tr>");
+            out.print("<td>"+user.getName()+"</td>");
+            out.print("<td>"+user.getSurname()+"</td>");
+            out.print("<td>"+user.getDepartment()+"</td>");
+            out.print("<td>"+user.getSalary()+"</td>");
+            out.print("</tr>");
         }
+
+        out.print("</table>");
     }
-
-
 }
